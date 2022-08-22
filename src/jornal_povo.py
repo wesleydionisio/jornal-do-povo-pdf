220ffc8e78f2e143e99832f5c3daae095101ad31
Wesley Dionisio <wesley.r.dionisio@gmail.com>
2022-08-22 09:24:58 -0300

    Pequenos ajustes na sintaxe

--- src/jornal_povo.py
+++ src/jornal_povo.py
@@ -1,19 +1,21 @@
+from http.client import OK
 import requests
 
 
 class JornalPovo:
     def __init__(self) -> None:
-        self.url_base = 'http://www.jornaldopovo.net'
+        self.url_base = 'https://www.jornaldopovo.net'
 
     def get_pages(self, date, day):
-        prefix = '_copy'
+        prefix = ["_copy", "", "copy"]
+        counterPrefix = 0
         keep_looking = True
         pages = []
         counter = 1
-        objetive = 0 #There's definitely a better way to do this.
+        objetive = False
 
         while keep_looking:
-            url = f'{self.url_base}/cliente/img/edicao/{date}/jpg/{day}pg{str(counter).zfill(2)}{prefix}.jpg'
+            url = f'{self.url_base}/cliente/img/edicao/{date}/jpg/{day}pg{str(counter).zfill(2)}{prefix[counterPrefix]}.jpg'
 
             try:
                 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
@@ -22,10 +24,13 @@ class JornalPovo:
 
                 if r.ok and content_type.startswith('image'):
                     pages.append(url)
-                elif r.ok == False and objetive == 0:
-                    prefix = ''
-                    objetive = 1
+                    objetive = True
+                    
+                elif r.ok == False and objetive == False:
+                    if counterPrefix < len(prefix):
+                        counterPrefix += 1
                     counter = 0
+                    
                 else:
                     keep_looking = False
             except:
